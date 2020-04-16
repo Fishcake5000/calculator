7cd1f7904d9757d875a7a077eab20923c69f1313
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2020-04-16 23:57:02 +0200

    Added simple display feedback

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ operators.forEach(operator => operator.addEventListener('click', newOperator));
 functions.forEach(funct => funct.addEventListener('click', newFunction));
 
 function newNumber(e) {
-    
+    display.textContent += e.target.textContent;
     return;
 }
 
 function newOperator(e) {
+    display.textContent += e.target.textContent;
     return;
 }
 
 function newFunction(e) {
+    display.textContent += e.target.textContent;
     return;
 }
