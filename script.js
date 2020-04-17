65c724ee63d362250892abeb5b3896c643854fb7
Maxime Arnaudy <maxime.arnaudy@gmail.com>
2020-04-17 16:12:12 +0200

    Added keyboard support

--- script.js
+++ script.js
@@ -2,39 +2,44 @@
 const display = document.querySelector('#display');
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
-numbers.forEach(number => number.addEventListener('click', newNumber));
-operators.forEach(operator => operator.addEventListener('click', newOperator));
+numbers.forEach(number => number.addEventListener('click', clickNumber));
+operators.forEach(operator => operator.addEventListener('click', clickOperator));
 const enterButton = document.querySelector('#enter');
 const clearButton = document.querySelector('#clear');
 const backButton = document.querySelector('#back');
 enterButton.addEventListener('click', enter);
 clearButton.addEventListener('click', clear);
 backButton.addEventListener('click', back);
+window.addEventListener('keydown', keyPress);
 
 
-function newNumber(e) {
-    calculation[calculation.length - 1] += e.target.textContent
-    display.textContent += e.target.textContent;
-    return;
+function clickNumber(e) {
+    newNumber(e.target.textContent);
 }
 
-function newOperator(e) {
-    calculation.push(e.target.textContent);
+function newNumber(str) {
+    calculation[calculation.length - 1] += str;
+    display.textContent += str;
+}
+
+function clickOperator(e) {
+    newOperator(e.target.textContent);
+}
+
+function newOperator(str) {
+    calculation.push(str);
     calculation.push('');
-    display.textContent += e.target.textContent;
-    return;
+    display.textContent += str;
 }
 
 function enter() {
     calculation = calculate(calculation);
     display.textContent = calculation[0];
-    return;
 }
 
 function clear() {
     calculation = [''];
     display.textContent = '';
-    return;
 }
 
 function back() {
@@ -45,7 +50,42 @@ function back() {
         calculation[calculation.length -1] = calculation[calculation.length -1].slice(0,-1);
     }
     display.textContent = display.textContent.slice(0,-1);
+}
+
+function addDecimal() {
     return;
+}
+
+function keyPress(e) {
+    switch (e.key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            newNumber(e.key);
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            newOperator(e.key);
+            break;
+        case 'Enter':
+            enter();
+            break;
+        case 'Backspace':
+            back();
+            break;
+        case '.':
+            addDecimal;
+            break;
+    }
 }
 
 isPriorityOperation = (str) => (str === '/' || str === '*');
@@ -54,8 +94,8 @@ isPriorityOperation = (str) => (str === '/' || str === '*');
 isLowPriorityOperation = (str) => (str === '+' || str === '-');
 
 function calculate(calculation) {
-    if (calculation.some(str => (str === '')) ) {
-        return 'ERROR';
+    if (calculation.some(str => (str === '') || str.search(/[a-zA-Z]/)!= -1 ) ) {
+        return ['ERROR'];
     }
     while (calculation.some(isPriorityOperation)) {
         const index = calculation.findIndex(isPriorityOperation);
@@ -65,7 +105,7 @@ function calculate(calculation) {
         } else {
             result = calculation[index-1] / calculation[index+1];
         }
-        calculation.splice(index-1, 3, result);
+        calculation.splice(index-1, 3, result.toString());
     }
     while (calculation.some(isLowPriorityOperation)) {
         const index = calculation.findIndex(isLowPriorityOperation);
@@ -75,7 +115,7 @@ function calculate(calculation) {
         } else {
             result = calculation[index-1] - calculation[index+1];
         }
-        calculation.splice(index-1, 3, result);
+        calculation.splice(index-1, 3, result.toString());
     }
     return calculation;
 }
